5c7b59ea08f15fac4ff9d58f64be8af3bb8476d6
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2025-01-08 18:53:16 +0900

    * gotop박스 조정

--- src/main/resources/static/js/navControl.js
+++ src/main/resources/static/js/navControl.js
@@ -9,13 +9,13 @@ function updateCategoryPosition() {
     if (window.innerWidth >= 1024) {
         // 데스크탑 화면에서는 위치 설정
         categoryModal.style.left = `${contentRect.right + 10}px`;
-        indexModal.style.left = `${contentRect.left - 190}px`;
         goTop.style.left = `${contentRect.right + 2}px`;
+        indexModal.style.left = `${contentRect.left - 190}px`;
     } else {
         // 모바일 화면에서는 위치 초기화
         categoryModal.style.left = '';
         indexModal.style.left = '';
-        goTop.style.left = '';
+        goTop.style.left = `${contentRect.right + 2}px`;
     }
 }
 
