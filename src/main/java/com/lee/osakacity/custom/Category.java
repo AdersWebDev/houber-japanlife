65c2e0dca419aba89e5097b5bd53e8b95fd1ba61
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2025-02-13 15:43:11 +0900

    카테고리 순서 변경

--- src/main/java/com/lee/osakacity/custom/Category.java
+++ src/main/java/com/lee/osakacity/custom/Category.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Category {
-    hot_post("おすすめ", "지금 가장 인기 있는 콘텐츠를 확인해보세요!"),
-    working_holiday("워킹 홀리데이", "일본 워킹 홀리데이를 위한 가이드를 모았습니다."),
-    japan_review("일본 생활 후기", "일본 생활의 생생한 후기! 실제 경험담을 통해 다양한 이야기를 만나보세요."),
+    hot_post("おすすめ", "하우버에서 가장 인기 있는 콘텐츠를 확인해보세요!"),
+    working_holiday("워킹 홀리데이", "일본 워킹홀리데이를 위한 가이드를 모았습니다."),
+    japan_review("하우버 입주 후기!", "하우버 이용자분들의 실제 경험담을 통해 다양한 이야기를 만나보세요."),
     japan_life("일본 생활 정보", "일본에서의 일상생활과 유용한 정보를 한눈에 확인하세요."),
     houber_sns("영상 콘텐츠", "하우버의 새로운 소식을 확인해보세요."),
     all("전체 보기", "하우버의 모든 콘텐츠를 한곳에서 확인하세요.");
