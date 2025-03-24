1c66a779dcd7e58d7d9ca39ea92c6704377badf0
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2025-03-24 16:40:49 +0900

    kakao log

--- src/main/java/com/lee/osakacity/ai/config/RedisConfig.java
+++ src/main/java/com/lee/osakacity/ai/config/RedisConfig.java
@@ -1,8 +1,11 @@
 package com.lee.osakacity.ai.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -10,9 +13,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.password}")
+    private String redisPw;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("127.0.0.1", 6379);
+        config.setPassword(RedisPassword.of(redisPw));
         return new LettuceConnectionFactory();
     }
 
