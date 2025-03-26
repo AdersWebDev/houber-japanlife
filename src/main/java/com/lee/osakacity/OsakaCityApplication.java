67c57b9eb77ae4380272622de189650ac5d44099
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2025-03-26 11:13:58 +0900

    피곤한가 왜이러지

--- src/main/java/com/lee/osakacity/OsakaCityApplication.java
+++ src/main/java/com/lee/osakacity/OsakaCityApplication.java
@@ -2,8 +2,10 @@ package com.lee.osakacity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class OsakaCityApplication {
 
     public static void main(String[] args) {
